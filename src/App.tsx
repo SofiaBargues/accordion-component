b4bb0f30447a77de9442a6bb27896e44a1b54a71
Sofia Bargues <barguessofia@gmail.com>
2024-08-07 13:42:52 +0100

    style

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,64 @@
 import { useState } from "react";
 
+function Button({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick: () => void;
+}) {
+  return (
+    <button
+      className="bg-[#efefef] w-full text-start border border-[#efefef]"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
 function App() {
   const [clicked1, isClicked1] = useState(false);
   const [clicked2, isClicked2] = useState(false);
 
   return (
     <>
-      <div className="flex justify-center h-screen flex-col ">
-        <button
-          className="m-6 border-black border"
-          onClick={() => isClicked1(!clicked1)}
-          id="1"
+      <div className="flex items-start flex-col m-6 font-semibold">
+        <Button
+          onClick={() => {
+            isClicked1(!clicked1);
+            isClicked2(false);
+          }}
         >
-          <div>Devtools Tech? 🤔</div>
-        </button>
-        {clicked1 === true ? (
-          <div>
-            The aim with Devtools Tech is to create a platform for Frontend
-            Engineers where we all can improve, invest in ourselves, and grow by
-            learning from high quality real world programming content. This is a
-            platform where you can practice actual interview questions, watch
-            courses, read blogs, and keep track of your progress across various
-            domains and topics.
-          </div>
-        ) : (
-          ""
-        )}
-        <button
-          className="m-6 border-black border"
-          onClick={() => isClicked2(!clicked2)}
-          id="2"
+          <div className="ml-7">Devtools Tech? 🤔</div>
+          {clicked1 === true ? (
+            <div className="bg-white p-7">
+              The aim with Devtools Tech is to create a platform for Frontend
+              Engineers where we all can improve, invest in ourselves, and grow
+              by learning from high quality real world programming content. This
+              is a platform where you can practice actual interview questions,
+              watch courses, read blogs, and keep track of your progress across
+              various domains and topics.
+            </div>
+          ) : (
+            ""
+          )}
+        </Button>
+        <Button
+          onClick={() => {
+            isClicked2(!clicked2);
+            isClicked1(false);
+          }}
         >
-          <div>Is it Free?</div>
-        </button>
-        {clicked2 === true ? (
-          <div>Yes, the platform and YouTube both are completely free!</div>
-        ) : (
-          ""
-        )}
+          <div className="ml-7">Is it Free?</div>
+          {clicked2 === true ? (
+            <div className="bg-white p-7">
+              Yes, the platform and YouTube both are completely free!
+            </div>
+          ) : (
+            ""
+          )}
+        </Button>
       </div>
     </>
   );
